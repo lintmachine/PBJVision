fa862c6a8204fbd0fc310535aae5255be19555af
cdann <cdann@lintmachine.com>
2016-09-05 17:28:18 -0700

    Fix target file extension issue with Framework project.

---
+++ Project/VisionFramework/VisionFramework.h
@@ -0,0 +1,19 @@
+//
+//  VisionFramework.h
+//  VisionFramework
+//
+//  Created by cdann on 9/5/16.
+//  Copyright © 2016 Patrick Piemonte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VisionFramework.
+FOUNDATION_EXPORT double VisionFrameworkVersionNumber;
+
+//! Project version string for VisionFramework.
+FOUNDATION_EXPORT const unsigned char VisionFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisionFramework/PublicHeader.h>
+
+
