c3fff49ee97ce4e02ec4e6fdb6f9c1322e346273
cdann <cdann@lintmachine.com>
2016-09-05 17:08:12 -0700

    Add a Framework target for the library.

---
+++ Project/PBJVision/PBJVision.h
@@ -0,0 +1,19 @@
+//
+//  PBJVision.h
+//  PBJVision
+//
+//  Created by cdann on 9/5/16.
+//  Copyright © 2016 Patrick Piemonte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PBJVision.
+FOUNDATION_EXPORT double PBJVisionVersionNumber;
+
+//! Project version string for PBJVision.
+FOUNDATION_EXPORT const unsigned char PBJVisionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PBJVision/PublicHeader.h>
+
+
